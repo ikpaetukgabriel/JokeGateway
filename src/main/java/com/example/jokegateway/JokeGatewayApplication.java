bd15c64f92dc1e12ee962d2490186c7f3aff436b
gabrielikpaetuk <gabrielikpaetuk@byui.edu>
2021-12-09 17:38:25 -0700

    Initial Commit

---
+++ src/main/java/com/example/jokegateway/JokeGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.jokegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JokeGatewayApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/jokegateway/JokeGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jokegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeGatewayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
